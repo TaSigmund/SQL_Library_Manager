1e5263be3f27d90f3b3094867aa7376885546317
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-03-22 22:31:46 +0100

    model sync

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//db
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'library.db'
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }})()
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
